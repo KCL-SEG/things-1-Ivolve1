5c70f1d2c679229e4f347de28a5d79eee343a029
Ivo Verneris <ivo.verneris@kcl.ac.uk>
2022-10-07 03:55:16 +0100

    Add Thing model and refined it

--- things/models.py
+++ things/models.py
@@ -1,3 +1,20 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class Thing(Model):
+    name = models.CharField(
+        max_length = 30,
+        unique=True,
+        blank = False
+    )
+    description = models.CharField(
+        max_length = 120,
+        unique = False,
+        blank = True
+    )
+    quantity = models.IntegerField(
+        unique = False,
+        validators = [MinValueValidator(0, "Quantity must not be less than 0"),
+        MaxValueValidator(100, "Quantity must not be more than 100")]
+    )
